ab42f732031d63a7f4c7030a34e7162db67fb5db
Földi Tamás <tfoldi@xsi.hu>
2024-03-02 10:44:31 +0100

    bug: add missing header

--- Go2_ROS2_example/src/src/record_bag.cpp
+++ Go2_ROS2_example/src/src/record_bag.cpp
@@ -8,6 +8,7 @@
 
 #include "rosbag2_cpp/writer.hpp"
 #include "rosbag2_cpp/writers/sequential_writer.hpp"
+#include <rosbag2_cpp/storage_options.hpp>
 #include "rosbag2_storage/serialized_bag_message.hpp"
 
 #define HIGH_FREQ 0
