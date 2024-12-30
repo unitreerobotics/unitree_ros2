c0e460f4f5cc49a4b2e9b6671bd854337771bf6d
yangning wu <rd_wyn@unitree.com>
2024-12-30 20:31:25 +0800

    update h1-2 lowlevel example

--- example/src/include/common/motor_crc_hg.h
+++ example/src/include/common/motor_crc_hg.h
@@ -11,42 +11,6 @@
 #include "unitree_hg/msg/low_cmd.hpp"
 #include "unitree_hg/msg/motor_cmd.hpp"
 
-constexpr int LeftHipPitch = 0;
-constexpr int LeftHipRoll = 1;
-constexpr int LeftHipYaw = 2;
-constexpr int LeftKnee = 3;
-constexpr int LeftAnklePitch = 4;
-constexpr int LeftAnkleB = 4;
-constexpr int LeftAnkleRoll = 5;
-constexpr int LeftAnkleA = 5;
-constexpr int RightHipPitch = 6;
-constexpr int RightHipRoll = 7;
-constexpr int RightHipYaw = 8;
-constexpr int RightKnee = 9;
-constexpr int RightAnklePitch = 10;
-constexpr int RightAnkleB = 10;
-constexpr int RightAnkleRoll = 11;
-constexpr int RightAnkleA = 11;
-constexpr int WaistYaw = 12;
-constexpr int WaistRoll = 13;	 // NOTE INVALID for g1 23dof/29dof with waist locked
-constexpr int WaistA = 13;	 // NOTE INVALID for g1 23dof/29dof with waist locked
-constexpr int WaistPitch = 14; // NOTE INVALID for g1 23dof/29dof with waist locked
-constexpr int WaistB = 14;	 // NOTE INVALID for g1 23dof/29dof with waist locked
-constexpr int LeftShoulderPitch = 15;
-constexpr int LeftShoulderRoll = 16;
-constexpr int LeftShoulderYaw = 17;
-constexpr int LeftElbow = 18;
-constexpr int LeftWristRoll = 19;
-constexpr int LeftWristPitch = 20; // NOTE INVALID for g1 23dof
-constexpr int LeftWristYaw = 21;	 // NOTE INVALID for g1 23dof
-constexpr int RightShoulderPitch = 22;
-constexpr int RightShoulderRoll = 23;
-constexpr int RightShoulderYaw = 24;
-constexpr int RightElbow = 25;
-constexpr int RightWristRoll = 26;
-constexpr int RightWristPitch = 27; // NOTE INVALID for g1 23dof
-constexpr int RightWristYaw = 28;	  // NOTE INVALID for g1 23dof
-
 typedef struct
 {
 	uint8_t mode; // desired working mode
