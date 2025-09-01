e74adfb357ffcd9f635aee12627a0e9b441f6a86
sljunkie <lmello.009@gmail.com>
2025-09-01 12:07:09 -0300

    1. fix 'warning: backslash-newline at end of file' warning 2. source '/opt/ros/$ROS_DISTRO/setup.bash' instead of    '/opt/ros/foxy/setup.bash' (for humble and later)

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "Setup unitree ros2 environment"
-source /opt/ros/foxy/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 source $HOME/unitree_ros2/cyclonedds_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+# Modify INTERFACE_NAME to the network interface in which the unitree robot is connected
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-                            <NetworkInterface name="enp3s0" priority="default" multicast="default" />
+                            <NetworkInterface name="INTERFACE_NAME" priority="default" multicast="default" />
                         </Interfaces></General></Domain></CycloneDDS>'
